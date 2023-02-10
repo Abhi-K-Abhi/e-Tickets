519e0bfae6dce851b6c70a86c8bbf6c049c127da
abhi.patel <abhi.patel@ATPL-DSK-86>
2023-02-10 11:52:15 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace e_Tickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
